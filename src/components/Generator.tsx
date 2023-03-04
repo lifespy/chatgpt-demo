f70c63b6b5ad5ab895e97af9f3949d884d3cc7f5
lalala <julong2@gmail.com>
2023-03-04 16:57:11 +0800

    i

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -15,8 +15,8 @@ export default () => {
         if (temp < lastScrollTop){
             scroll = false
         }
-        let a = document.getElementsByClassName('message-list')[0].clientHeight + document.getElementsByClassName('message-list')[0].scrollTop
-        if (Math.abs(a - document.getElementsByClassName('message-list')[0].scrollHeight) < 10){
+        let height = document.getElementsByClassName('message-list')[0].clientHeight + document.getElementsByClassName('message-list')[0].scrollTop
+        if (Math.abs(height - document.getElementsByClassName('message-list')[0].scrollHeight) < 10){
             scroll = true
         }
         lastScrollTop = temp
